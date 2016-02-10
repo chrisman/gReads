9f8d9864cb54452bfbffa12cac492539b627b2bf
Christopher Brown <christopher.p.brown@gmail.com>
2016-02-10 08:00:13 -0700

    book validation

--- test/validate_book.js
+++ test/validate_book.js
@@ -1,5 +1,28 @@
 var chai = require('chai');
+var assert = require('assert');
+var book = require('../lib/validate_book')
 
 describe('has errors', function(){
-  it('should not allow empty fields');
+  it('title must not be empty', function(){
+    assert.equal(book.has_errors({
+      title: '',
+      cover_url: 'this url',
+      description: 'this desc'
+    }), ['Title cannot be blank']);
+  });
+
+  it('cover_url must not be emtpy', function(){
+    assert.equal(book.has_errors({
+      title: 'this title',
+      cover_url: ''
+    }), ['Cover URL cannot be blank']);
+  });
+
+  it('description must not be emtpy', function(){
+    assert.equal(book.has_errors({
+      title: 'this title',
+      cover_url: 'this url',
+      description: ''
+    }), ['Description cannot be blank']);
+  });
 });
