eb30d4e912cb00ad08c0ea6220486b76dde22d42
Christopher Brown <christopher.p.brown@gmail.com>
2016-02-10 11:02:24 -0700

    nav styling. random lib

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../lib/db/books');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'READ A BOOK' });
+  db.getRandomBook(function(book){
+    res.render('index', {
+      book: book
+    });
+  });
 });
 
 module.exports = router;
---
+++ test/random.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var assert = require('assert');
+var random = require('../lib/random')
+
+describe('random', function(){
+  xit('returns a random value from an array', function(){
+  });
+});
